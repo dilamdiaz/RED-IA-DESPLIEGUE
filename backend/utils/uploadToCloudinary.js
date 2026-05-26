36f8695916ae94b3a6701eeec314f8979626e2ff
dilamdiaz <dilamdiazv@gmail.com>
2026-05-26 18:43:37 -0500

    fix: Preview Cloudinary

--- backend/utils/uploadToCloudinary.js
+++ backend/utils/uploadToCloudinary.js
@@ -4,12 +4,22 @@ const uploadToCloudinary = async (fileBuffer, mimetype) => {
 
   return new Promise((resolve, reject) => {
 
-    const resourceType =
-      mimetype.includes('image')
-        ? 'image'
-        : mimetype.includes('video')
-        ? 'video'
-        : 'raw';
+    let resourceType = 'raw';
+
+    // 🖼️ imágenes
+    if (mimetype.includes('image')) {
+      resourceType = 'image';
+    }
+
+    // 📄 PDFs
+    else if (mimetype.includes('pdf')) {
+      resourceType = 'image';
+    }
+
+    // 🎥 videos
+    else if (mimetype.includes('video')) {
+      resourceType = 'video';
+    }
 
     cloudinary.uploader.upload_stream(
       {
